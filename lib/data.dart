29195b1956a1267c489c9ed00413c9c49b7e456e
gusanniecl <gusanniecl@student.gu.se>
2022-10-17 09:12:43 +0200

    Liten förändring i categories klassen.

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,11 @@
 // Klass för att skapa de olika kategorierna
+
 class Categories {
-  final int position;
   final String name;
   final String iconImage;
   final String description;
 
-  Categories(
-    this.position, {
+  Categories({
     required this.name,
     required this.iconImage,
     required this.description,
@@ -14,35 +13,35 @@ class Categories {
 }
 
 List<Categories> categories = [
-  Categories(1,
+  Categories(
       name: 'DAD JOKES',
       iconImage: 'https://cdn-icons-png.flaticon.com/128/7251/7251743.png',
       description: 'A list with dad jokes'),
-  Categories(2,
+  Categories(
       name: 'RANDOM FACTS',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/5619/5619947.png',
       description: 'A list with random facts'),
-  Categories(3,
+  Categories(
       name: 'EXCUSES',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/686/686751.png',
       description: 'A list with excuses'),
-  Categories(4,
+  Categories(
       name: 'YO MOMMA JOKES',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/686/686751.png',
       description: 'A list with yo momma jokes'),
-  Categories(5,
+  Categories(
       name: 'PICK UP LINES',
       iconImage: 'https://cdn-icons-png.flaticon.com/128/677/677684.png',
       description: 'A list with pick up lines'),
-  Categories(6,
+  Categories(
       name: 'CHUCK NORRIS JOKES',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/4383/4383956.png',
       description: 'A list with Chuck Norris jokes'),
-  Categories(7,
+  Categories(
       name: 'GEEK JOKES',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/6601/6601160.png',
       description: 'A list with geek jokes'),
-  Categories(8,
+  Categories(
       name: 'MEMES',
       iconImage: 'https://cdn-icons-png.flaticon.com/512/3475/3475926.png',
       description: 'A list with memes'),
